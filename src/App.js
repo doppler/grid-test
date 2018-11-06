72fa42011cd23d35af8869eeace0cd9c51bb8d02
David Rose <doppler@gmail.com>
2018-11-05 23:56:25 -0500

    nice grid layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="Header">
+          <h1>Header</h1>
+        </div>
+        <div className="Sidebar">
+          <h2>Sidebar</h2>
+          <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+            <li>Four</li>
+          </ul>
+        </div>
+        <div className="Content">
+          <h2>Content</h2>
+        </div>
+        <div className="Footer">
+          <h2>Footer</h2>
+        </div>
       </div>
     );
   }
